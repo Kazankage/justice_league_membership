cc6a30e4425620a4d7425932e7ea0e2375992db7
kazankage <randeljiao@gmail.com>
2020-06-01 15:46:03 -0700

    controller

--- membership-api-backend/app/controllers/members_controller.rb
+++ membership-api-backend/app/controllers/members_controller.rb
@@ -1,2 +1,39 @@
 class MembersController < ApplicationController
+    def index
+        @members = Member.all
+
+        render json: @members
+    end
+
+    def show
+        @member = find(params[:id])
+
+        render json: @member
+    end
+
+    def create
+        @member.create(member_params)
+
+        render json: @member
+    end
+
+    def update
+        @member = find(params[:id])
+        @member.update(member_params))
+
+        render json: @member
+    end
+
+    def destroy
+        @member = find(params[:id])
+        @member.delete
+
+        render json: {memberId: @member.id}
+    end
+
+    private
+
+    def member_params
+        params.require(:member).permit(:name, :powers, :membership)
+    end
 end
